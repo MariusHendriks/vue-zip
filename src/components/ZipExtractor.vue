ff13833fcdea6c4c09aefd23552fadff57525ced
Mark Hendriks <mark.hendriks9999@gmail.com>
2024-05-24 21:33:01 +0200

    add groups, add download per group

--- src/components/ZipExtractor.vue
+++ src/components/ZipExtractor.vue
@@ -1,25 +1,36 @@
 <template>
   <div class="zip-extractor">
     <h1>Zip extractor</h1>
+
     <input type="file" @change="handleFileSelect" />
     <div v-if="fileList.length > 0" class="files">
       <h1>Files:</h1>
-
       <div v-for="file in fileList" :key="file.name" class="file">
         {{ file.name }}
         <button @click="downloadFile(file)">Download</button>
       </div>
-
       <button class="download-all" @click="downloadAllFiles">Download All Files</button>
+
+      <div v-for="(groupFiles, groupName) in groupedFiles" :key="groupName">
+        <h2>{{ groupName }}</h2>
+        <ul>
+          <li v-for="(file, index) in groupFiles" :key="file.name">
+            {{ file.name }}
+            <button @click="downloadFileFromGroup(groupName, index)">Download</button>
+          </li>
+          <button @click="downloadGroup(groupName)">Download All {{ groupName }} Files</button>
+        </ul>
+      </div>
+
       <a ref="downloadLink" v-show="false" download></a>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
 import type { ZipItem } from '@/models/ZipItem'
 import JSZip from 'jszip'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const fileList = ref<ZipItem[]>([])
 const downloadLink = ref<HTMLAnchorElement | null>(null)
@@ -43,7 +54,6 @@ const handleFileSelect = async (event: Event) => {
           files.push({ name: key, file: fileData })
         }
       }
-
       fileList.value = files
     } else {
       alert('Please upload a valid zip file.')
@@ -61,6 +71,14 @@ const downloadFile = (file: ZipItem) => {
   }
 }
 
+const downloadFileFromGroup = (groupName: string, index: number) => {
+  const groupFiles = groupedFiles.value[groupName]
+  if (groupFiles && index >= 0 && index < groupFiles.length) {
+    const file = groupFiles[index]
+    downloadFile(file)
+  }
+}
+
 const downloadAllFiles = () => {
   fileList.value.forEach((fileItem) => {
     const url = URL.createObjectURL(fileItem.file)
@@ -72,6 +90,30 @@ const downloadAllFiles = () => {
     }
   })
 }
+
+const downloadGroup = (groupName: string) => {
+  const groupFiles = groupedFiles.value[groupName]
+  if (groupFiles) {
+    groupFiles.forEach(downloadFile)
+  }
+}
+
+const groupedFiles = computed(() => {
+  const groups: Record<string, ZipItem[]> = {}
+
+  fileList.value.forEach((file) => {
+    const parts = file.name.split('/')
+    const groupName = parts.length > 1 ? parts[0] : 'files'
+
+    if (!groups[groupName]) {
+      groups[groupName] = []
+    }
+
+    groups[groupName].push(file)
+  })
+
+  return groups
+})
 </script>
 
 <style scoped>
