e3d13609b0c10a683f79944e28de8b77d6cfdf7f
Mark Hendriks <mark.hendriks9999@gmail.com>
2024-05-24 21:44:35 +0200

    add a bunch of styling

--- src/components/ZipExtractor.vue
+++ src/components/ZipExtractor.vue
@@ -1,29 +1,37 @@
 <template>
   <div class="zip-extractor">
-    <h1>Zip extractor</h1>
+    <div clas="upload">
+      <h1>Zip extractor</h1>
 
-    <input type="file" @change="handleFileSelect" />
-    <div v-if="fileList.length > 0" class="files">
-      <h1>Files:</h1>
-      <div v-for="file in fileList" :key="file.name" class="file">
-        {{ file.name }}
-        <button @click="downloadFile(file)">Download</button>
-      </div>
-      <button class="download-all" @click="downloadAllFiles">Download All Files</button>
-
-      <div v-for="(groupFiles, groupName) in groupedFiles" :key="groupName">
-        <h2>{{ groupName }}</h2>
-        <ul>
-          <li v-for="(file, index) in groupFiles" :key="file.name">
-            {{ file.name }}
-            <button @click="downloadFileFromGroup(groupName, index)">Download</button>
-          </li>
-          <button @click="downloadGroup(groupName)">Download All {{ groupName }} Files</button>
-        </ul>
-      </div>
-
-      <a ref="downloadLink" v-show="false" download></a>
+      <input class="upload" type="file" @change="handleFileSelect" />
     </div>
+    <div v-if="fileList.length > 0" class="files-container">
+      <div class="files">
+        <h1>All Files:</h1>
+        <div v-for="file in fileList" :key="file.name" class="file">
+          {{ file.name }}
+          <button @click="downloadFile(file)">Download</button>
+        </div>
+        <button class="download-all" @click="downloadAllFiles">Download All Files</button>
+      </div>
+
+      <div v-if="fileList !== groupedFiles.value">
+        <h1>Files per directory:</h1>
+        <div v-for="(groupFiles, groupName) in groupedFiles" :key="groupName">
+          <div class="files">
+            <h3>{{ groupName }}</h3>
+            <div v-for="(file, index) in groupFiles" :key="file.name" class="file">
+              {{ file.name }}
+              <button @click="downloadFileFromGroup(groupName, index)">Download</button>
+            </div>
+            <button class="download-all" @click="downloadGroup(groupName)">
+              Download All {{ groupName }} Files
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a ref="downloadLink" v-show="false" download></a>
   </div>
 </template>
 
@@ -103,7 +111,7 @@ const groupedFiles = computed(() => {
 
   fileList.value.forEach((file) => {
     const parts = file.name.split('/')
-    const groupName = parts.length > 1 ? parts[0] : 'files'
+    const groupName = parts.length > 1 ? parts[0] : 'root'
 
     if (!groups[groupName]) {
       groups[groupName] = []
@@ -123,8 +131,11 @@ const groupedFiles = computed(() => {
   align-items: center;
 }
 
+.files-container {
+  display: flex;
+  gap: 5em;
+}
 .files {
-  margin-top: 30px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -140,5 +151,9 @@ const groupedFiles = computed(() => {
 
 .download-all {
   margin-top: 15px;
+}
+
+.upload {
+  margin-bottom: 30px;
 }
 </style>
